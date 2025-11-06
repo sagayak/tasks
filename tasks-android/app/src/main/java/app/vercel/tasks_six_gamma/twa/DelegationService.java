3e8905733099b407da18997864219bb706d24950
pinkysan0312-eng <pinkysan0312@gmail.com>
2025-11-06 20:44:20 +0530

    abc

---
+++ tasks-android/app/src/main/java/app/vercel/tasks_six_gamma/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.tasks_six_gamma.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
